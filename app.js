9d6d1bb3671bb5ef34e5fef34192b4e4e31878fe
Ivan <ivangutsalenko92@gmail.com>
2025-01-18 17:33:37 +0200

    Move mongo db string to .env file

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-
-const MONGODB_URI =
-    'mongodb+srv://ivangutsalenko92:70biPAYmrs24HVGz@cluster0.bsq4a.mongodb.net/formula1?retryWrites=true&w=majority&appName=Cluster0';
 
 const driverRoutes = require('./routes/drivers');
 const userRoutes = require('./routes/user');
@@ -19,7 +17,7 @@ app.use(driverRoutes);
 app.use(userRoutes);
 
 mongoose
-    .connect(MONGODB_URI)
+    .connect(process.env.MONGO_URL)
     .then(() => {
         app.listen(8080);
     })
